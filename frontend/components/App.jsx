60729e539451af1eb0ee28bb6e22858f248cab5d
Conor Canton <conorstreete@gmail.com>
2024-04-02 23:31:58 +0800

    created a basic card and linked vite and react with shopify

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,45 @@
+import { Card, Stack,ButtonGroup,ToggleButton } from 'react-bootstrap'
+import { useState } from 'react';
 export default function App({ home }) {
   console.log('Home', home)
+  const [checked, setChecked] = useState(false);
+  const [weaponType, setWeaponType] = useState('');
 
   return (
-    <div className="tw-text-5xl tw-text-red-600">
-      Hello From React! test
+    <div className="tw-text-5xl">
+      <Card style={{ borderWidth:'3px',width: '100%',height:'200px',verticalAlign:'center',display:'flex', borderColor:'#9de3ef' }}>
+        <Card.Body>
+          <Card.Title style={{fontSize:'20px',color:'#dfb990'}}>Choose Our Next Adventure:</Card.Title>
+          <Stack className="col-md-12" direction="horizontal" gap={3} style={{width:'100%',height:'75%'}}>
+          
+        <ToggleButton
+          id="toggle-check"
+          type="checkbox"
+          variant="outline-light"
+          checked={ weaponType == 'spear'}
+          value='spear'
+          style={{height:'100%',width:'100%'}}
+          onChange={(e) => setWeaponType(e.currentTarget.value)}
+        >
+          <img style={{}}src='https://cdn-icons-png.freepik.com/512/5778/5778627.png'></img>
+        </ToggleButton>
+
+        <ToggleButton
+          id="toggle-check1"
+          type="checkbox"
+          variant="outline-light"
+          checked={ weaponType == 'knife'}
+          value='knife'
+          style={{height:'100%',width:'100%'}}
+          onChange={(e) => setWeaponType(e.currentTarget.value)}
+        >
+          <img style={{height:'100%'}}src='https://parspng.com/wp-content/uploads/2022/07/knifepng.parspng.com-8.png'></img>
+        
+        </ToggleButton>
+        
+           </Stack>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import shopify from 'vite-plugin-shopify'
 import react from '@vitejs/plugin-react'
+import pageReload from 'vite-plugin-page-reload'
+import ViteRestart from 'vite-plugin-restart'
+
 
 export default defineConfig({
   plugins: [
     shopify({
       themeRoot: 'extensions/test-vite-app'
     }),
+    pageReload('./extensions','extensions/test-vite-app/snippets/vite-tag.liquid'),
     react(),
+    ViteRestart({
+      restart: [
+        './frontend/components/*.*',
+      ]
+    })
   ]
 })
