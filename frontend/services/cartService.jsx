a7ee044115a9ceee29e32d0831ca86c76348bd4e
Conor Canton <conorstreete@gmail.com>
2024-04-03 02:32:53 +0800

    added new cart service

--- frontend/services/cartService.jsx
+++ frontend/services/cartService.jsx
@@ -2,26 +2,26 @@
 const fetchCart = async () => {
     const response = await fetch(window.Shopify.routes.root + 'cart.js');
     const cart = await response.json();
-  
+
     return cart;
 }
 
-const updateCartAttributes = async () => {
-    
+const updateCartAttributes = async (cartData) => {
+    await fetch(window.Shopify.routes.root + 'cart/add.js', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(cartData)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
 
-fetch(window.Shopify.routes.root + 'cart/add.js', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  })
-  .then(response => {
-    return response.json();
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
 
-  export {fetchCart}
+
+export { fetchCart,updateCartAttributes }
